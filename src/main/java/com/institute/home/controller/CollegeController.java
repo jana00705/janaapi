6b603a4cf0b5865a1ef8911fc91cdfc4012f6c34
jana00705 <janadexter007@gmail.com>
2021-10-05 18:59:22 +0530

    api

--- src/main/java/com/institute/home/controller/CollegeController.java
+++ src/main/java/com/institute/home/controller/CollegeController.java
@@ -1,14 +1,19 @@
 package com.institute.home.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.institute.home.modal.College;
+import com.institute.home.modal.Student;
 import com.institute.home.repository.CollegeRepository;
 
 @RestController
@@ -28,4 +33,29 @@ public class CollegeController {
 				.body("College Registered Successfully");
 		
 	}
+
+	@GetMapping(value = "/getAll")
+	public ResponseEntity<?> getAllColleges(){
+		ArrayList<College> colleges = (ArrayList<College>) CollegeRepo.findAll();
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(colleges);
+	}
+	
+	@GetMapping(value = "/delete/{id}")
+	public ResponseEntity<?> deleteCollege(@PathVariable final int id){
+		CollegeRepo.deleteById(id);
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body("College deleted Successfully!");
+		
+	}
+	
+	@GetMapping(value = "/get/{id}")
+	public ResponseEntity<?> getCollege(@PathVariable final int id){
+		College college = CollegeRepo.findById(id).get();
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(college);
+	}	
 }
--- src/main/java/com/institute/home/controller/StudentController.java
+++ src/main/java/com/institute/home/controller/StudentController.java
@@ -1,8 +1,11 @@
 package com.institute.home.controller;
 
+import java.util.ArrayList;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +30,13 @@ public class StudentController {
 				.status(HttpStatus.OK)
 				.body("Student Registered Successfully");
 		
+	}
+	
+	@GetMapping(value = "/getAll")
+	public ResponseEntity<?> getAllStudents(){
+		ArrayList<Student> students = (ArrayList<Student>) StudentRepo.findAll();
+		return ResponseEntity
+				.status(HttpStatus.OK)
+				.body(students);
 	}
 }
